30c5cb9c7f08964851d4ef73d322884315630041
rodrip13 <rodrip137@gmail.com>
2020-08-13 08:26:22 -0300

    Lista de productos

--- js/products.js
+++ js/products.js
@@ -1,6 +1,48 @@
+//Definimos variable que va a tomar los datos desde el JSON especificado en url: PRODUCTS_URL
+var currentProductsArray = [];
+
+
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let products = currentProductsArray[i];
+
+            htmlContentToAppend += `
+            <a href="category-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + products.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + products.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+
+        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            console.log(resultObj.data);
+            currentProductsArray = resultObj.data;
+            showProductsList();
+        }
+    });
+
 
 });
