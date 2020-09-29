358eb63c5efc5709b0cf3fb0939e83c97851aafc
rodrip13 <rodrip137@gmail.com>
2020-09-29 11:54:26 -0300

    carrito de compras

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,39 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function showCart(array) {
+    let mostrar = document.getElementById("mostrarInfo");
+
+
+    let nombre = array.articles[0].name;
+    let img = array.articles[0].src;
+    let cantidad = array.articles[0].count;
+    let costo = array.articles[0].unitCost;
+    let moneda = array.articles[0].currency;
+
+    mostrar.innerHTML += `
+    <h1>Nombre Prodcuto</h1>
+    <div id="nombreProducto"> ` + nombre + `</div>
+    <h2>cantidad</h2>
+    <div id="cantidad">` + cantidad + `</div>
+    <h2>Precio por unidad</h2>
+    <div id="precio">` + moneda + `  ` + costo + `</div>
+    <img id="imagenProducto" src="` + img + `" alt="">
+    `
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e) {
+
+    getJSONData(CART_INFO_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            var info = resultObj.data;
+            //showProductsRelated(products);
+            showCart(info);
+
+        }
+    });
 
 });
